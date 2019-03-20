ee64c053dac5fd917924b37f3c4d75eddc37fcf2
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-19 23:59:15 -0700

    .use() notes

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const path = require("path"); //node.js module to deal with file paths
 
 const app = express();
 
+/* 
 app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+}); 
+*/
+// we can load HTML files this way, but not ideal
+// we'd have to put a route manually for every page
 
-// .send() - sends text to the browser
+// express comes w/ functionality to make a folder a static folder
+/* === Set a static folder === */
+app.use(express.static(path.join(__dirname, "public")));
+// .use() is used when we want to include MW
 
 const PORT = process.env.PORT || 5000;
 // when we deploy the server will most likely
