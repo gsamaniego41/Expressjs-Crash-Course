458ddfe340319209103eb0cce3725b034d1ca594
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-20 23:29:09 -0700

    express router

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const members = require("./Members");
 const logger = require("./middlware/logger");
 const app = express();
 
@@ -8,27 +7,11 @@ const app = express();
 // in order to use the logger MW,
 app.use(logger);
 
-// Gets all members
-app.get("/api/members", (req, res) => {
-  res.json(members);
-});
-
-// Get single member
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id));
-  /* Array.prototype.some()
-  The some() method tests whether at least one element in the array
-  passes the test implemented by the provided function.
-  */
-  if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({msg: `No member with the id of ${req.params.id}`});
-  }
-});
-
 /* === Set a static folder === */
 app.use(express.static(path.join(__dirname, "public")));
+
+// Members API Routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
