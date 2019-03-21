088a98cd0dabbc20ee4dc66e85d28abd26610d29
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-20 23:23:09 -0700

    gets a single member

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.get("/api/members", (req, res) => {
   res.json(members);
 });
 
+// Get single member
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+  /* Array.prototype.some()
+  The some() method tests whether at least one element in the array
+  passes the test implemented by the provided function.
+  */
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+  }
+});
+
 /* === Set a static folder === */
 app.use(express.static(path.join(__dirname, "public")));
 
